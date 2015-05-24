1b195269bf0d068a62e7a2cb031491a661b05550
tetujin <tetujin@ht.sfc.keio.ac.jp>
2015-05-24 15:12:17 +0900

    Initial Commit

---
+++ SenbayFormat/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SenbayFormat
+//
+//  Created by Yuuki Nishiyama on 2015/05/24.
+//  Copyright (c) 2015年 Yuuki NISHIYAMA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
